3b1491af8b1c2ecb36bbcd6bd4d43add9eb2e211
Tyler Bowers <tyler.j.bowers@gmail.com>
2017-10-05 08:20:19 -0600

    changed callback

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '32f6e2b53173461d9ab6cfdc5bd34786'; // Your client id
 var client_secret = '1d6ef338a60341ec9afed06d0ccb866d'; // Your secret
-var redirect_uri = 'http://tbowdev.com/prod/spotify-sampler/authorization_code/public'; // Your redirect uri
+var redirect_uri = 'http://tbowdev.com/prod/spotify-sampler'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
